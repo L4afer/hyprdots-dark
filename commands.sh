007441c01764704dfce1c834d7cca2eeedd727d7
L4aferr <leaferfr@gmail.com>
2026-04-12 20:01:03 +0200

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,7 @@
+yay -S waybar kitty nvim neofetch rofi
+cp -r ~/Downloads/hyprdots-dark/.config/nvim ~/.config
+cp -r ~/Downloads/hyprdots-dark/.config/waybar/ ~/.config
+cp -r ~/Downloads/hyprdots-dark/.config/rofi ~/.config/
+cp -r ~/Downloads/hyprdots-dark/.config/neofetch ~/.config
+cp -r ~/Downloads/hyprdots-dark/.config/kitty ~/.config 
+cp -r ~/Downloads/hyprdots-dark/.config/hypr ~/.config/
