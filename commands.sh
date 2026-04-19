fc492fc26eeb0a721252a40906f9daa21bf0e04a
L4aferr <leaferfr@gmail.com>
2026-04-19 02:28:44 +0200

    Add screenshot.sh to local bin and set permissions

--- commands.sh
+++ commands.sh
@@ -1,8 +1,7 @@
 yay -S waybar kitty nvim neofetch rofi 
 sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp -r ~/Downloads/hyprdots-dark/.config/nvim/screenshot.sh ~/.local/bin/
-sudo chmod +x ~/.local/bin/screenshot.sh
+
 cp -r ~/Downloads/hyprdots-dark/.config/nvim ~/.config
 cp -r ~/Downloads/hyprdots-dark/.config/waybar/ ~/.config
 cp -r ~/Downloads/hyprdots-dark/.config/rofi ~/.config/
@@ -11,4 +10,6 @@ sudo cp -r ~/Downloads/hyprdots-dark/.config/neofetch/neofetch /usr/bin/neofetch
 cp -r ~/Downloads/hyprdots-dark/.config/kitty ~/.config
 cp -r ~/Downloads/hyprdots-dark/.config/kitty/.zshrc ~/
 cp -r ~/Downloads/hyprdots-dark/.config/hypr ~/.config/
+cp -r ~/Downloads/hyprdots-dark/.config/hypr/screenshot.sh ~/.local/bin/
+sudo chmod +x ~/.local/bin/screenshot.sh
 nvim --headless "+Lazy! sync" +qa
